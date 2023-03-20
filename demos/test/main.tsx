4ebe68623ff8e4801c218a5d0ad33bf214f5ba3d
chulan <390226630@qq.com>
2023-03-20 17:19:24 +0800

    feat: 引入vite调试代码

---
+++ demos/test/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const jsx = <div>11 tiny-react 22</div>;
+
+const root = document.querySelector('#root');
+
+ReactDOM.createRoot(root).render(jsx);
+
+console.log(React);
+console.log(jsx);
+console.log(ReactDOM);
